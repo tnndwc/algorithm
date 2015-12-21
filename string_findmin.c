dffff8ba2bac64c444491879e93234fb38c6bc9b
Len <tnndwc@gmail.com>
2015-12-21 23:18:27 +0800

    manacher_12.20

---
+++ string_findmin.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+void quick_select(int s[], int k, int left, int right){
+    int i, j, pivot;//pivot 中数
+
+    if(left <= right){
+
+
+
+    }
+
+}
+
+int main(){
+
+    return 0;
+}
