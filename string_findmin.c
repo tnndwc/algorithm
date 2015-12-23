69018330fdfd99723b6e8eb346175e4151ae680e
Len <tnndwc@gmail.com>
2015-12-23 22:55:23 +0800

    manacher_12.24

--- string_findmin.c
+++ string_findmin.c
@@ -3,9 +3,38 @@
 #include <stdlib.h>
 #include "random_util.h"
 
-/*
 
-void quick_select(int s[], int k, int left, int right){
+int median3(int arr[], int left, int right){
+    int max = right - left;
+
+    int size = 3;
+    int *arr_ = (int *) malloc(sizeof(int) * size);
+    int *hash = (int *) malloc(sizeof(int) * (max + 1));
+
+    memset(hash, 0, sizeof(int) * (max + 1));
+
+    int index;
+    for (int i = 0; i < size; ++i) {
+
+        while ( hash[(index = (int) get_random_max(max))] > 0 ){
+            index = (int) get_random_max(max);
+        }
+
+        hash[index] = 1;
+        arr_[i] = arr[index];
+
+    }
+
+    for (int i = 0; i < size; ++i) {
+        printf("arr_ %d = %d\n", i, arr_[i]);
+    }
+
+    free(arr_);
+    free(hash);
+    return 0;
+}
+
+/*void quick_select(int s[], int k, int left, int right){
     int i, j, pivot;
 
     if(left <= right){
@@ -14,8 +43,8 @@ void quick_select(int s[], int k, int left, int right){
 
     }
 
-}
-*/
+}*/
+
 
 int main(){
 
@@ -25,6 +54,8 @@ int main(){
     int b = get_random_max(11);
     printf("random-max=%d\n", b);
 
+    int arr[] = {1, 3, 0, 100, 9, 7, 999};
+    median3(arr, 0, 6);
 
     return 0;
 }
