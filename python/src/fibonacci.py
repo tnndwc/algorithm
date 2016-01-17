47c2966a5d3606ccc56f54e459c8b58588f4c0fe
Len <tnndwc@gmail.com>
2016-01-17 16:06:44 +0800

    fibonacci

--- python/src/fibonacci.py
+++ python/src/fibonacci.py
@@ -31,6 +31,25 @@ def f2(n):
     return f2(n - 1) + f2(n - 2) + f2(n - 3)
 
 
+def f3(n):
+    """
+    递归法:最多一次跳 2 级, for方法
+    :param n:
+    :return:
+    """
+    if n <= 2:
+        return n
+    rs = [0, 1]
+    for i in xrange(n):
+        x = rs[1]
+        rs[1] += rs[0]
+        rs[0] = x
+
+    return rs[1]
+
+
 if __name__ == '__main__':
-    print 'f1:', f1(3)
+    print 'f1:', f1(6)
+    print 'f1:', f1(5)
     print 'f2:', f2(4)
+    print 'f3:', f3(6)
