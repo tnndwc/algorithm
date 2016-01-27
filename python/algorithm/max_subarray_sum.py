f44415179a2601290944f696daed5c6a98bfd4d7
Len <tnndwc@gmail.com>
2016-01-27 22:59:36 +0800

    max sub array sum

--- python/algorithm/max_subarray_sum.py
+++ python/algorithm/max_subarray_sum.py
@@ -6,5 +6,39 @@
 
 
 def sub_array_sum(num_arr):
-    for i in xrange(len(num_arr)):
-        print i
+    """
+    for 循环实现
+    :param num_arr:
+    :return: 最大的和
+    """
+    rs = 0
+    arr_len = len(num_arr)
+    for m in xrange(arr_len):
+        for i in xrange(m, arr_len):
+
+            sum = num_arr[i]
+            for n in xrange(i + 1, arr_len):
+                sum += num_arr[n]
+                if sum > rs:
+                    rs = sum
+    return rs
+
+
+def dynamic_sum(num_arr):
+    """
+    利用动态规划方法
+    :param num_arr:
+    :return: 最大的和
+    """
+    curr_sum = 0
+    max_sum = 0
+    for n in xrange(len(num_arr)):
+        if curr_sum >= 0:
+            curr_sum += num_arr[n]
+        else:
+            curr_sum = num_arr[n]
+
+        if curr_sum > max_sum:
+            max_sum = curr_sum
+
+    return max_sum
--- python/algorithm/max_subarray_sum_test.py
+++ python/algorithm/max_subarray_sum_test.py
@@ -1,4 +1,9 @@
 
+from max_subarray_sum import sub_array_sum, dynamic_sum
+
 num_arr = [1, 0, 100, -3, 10]
+print 'array length:', len(num_arr)
+print 'max sub array sum(for method):', sub_array_sum(num_arr)
+print 'max sub array sum(dynamic method):', dynamic_sum(num_arr)
 
 
